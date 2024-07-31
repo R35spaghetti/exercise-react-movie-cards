5dc6fcac6c8a60779c0c084e8c729a3051133b2d
RB <rkrb@tyfonmail.se>
2024-07-31 18:22:22 +0200

    CRUD movie collection

---
+++ data.tsx
@@ -0,0 +1,23 @@
+import {IMovie, IMovieCollection} from "./interfaces";
+
+export function AddMovie(movieCollection: IMovie[], newMovie: IMovie): IMovie[]
+{
+    return [...movieCollection, newMovie]
+}
+
+export const GetMovieCollection = (movies: IMovie[]): IMovieCollection => (
+    {
+        collection: movies
+    });
+
+export function SetMovie(setMovie: (movie: IMovie) => void, {id}: { id: number }) {
+    const {collection} = GetMovieCollection([]);
+    const targetMovie = collection.find(movie => movie.id === id);
+    if (targetMovie) {
+        setMovie(targetMovie);
+    }
+}
+
+export function DeleteMovieByIndex(movies: IMovie[], index: number): IMovie[] {
+    return movies.splice(index, 1);
+}
