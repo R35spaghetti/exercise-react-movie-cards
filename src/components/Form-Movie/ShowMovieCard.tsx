a7d0a53b95b6578c81646cd70faa19fca1c722c1
RB <rkrb@tyfonmail.se>
2024-08-01 23:57:36 +0200

    id becomes number

--- src/components/Form-Movie/ShowMovieCard.tsx
+++ src/components/Form-Movie/ShowMovieCard.tsx
@@ -4,22 +4,24 @@ import {ReactElement, useState} from "react";
 import {AddMovie} from "../../../data.ts";
 import {MovieCard} from "../Card-Movie/MovieCard.tsx";
 
+let idCount = 0;
 
 export function ShowMovieCard(): ReactElement {
 
     const [movieCollection, setMovieCollection] = useState<IMovie[]>([]);
-
     const handleSubmit = (data: IMovieForm) => {
         console.log(data);
         const movie: IMovie = {
-            id: Date.now(),
+            id: idCount,
             title: data.title,
             rating: data.rating,
             genre: data.genre,
             description: data.description,
         };
         const updatedMovieCollection = AddMovie(movieCollection, movie);
+        idCount++;
         setMovieCollection(updatedMovieCollection);
+
     };
     return (
         <>
