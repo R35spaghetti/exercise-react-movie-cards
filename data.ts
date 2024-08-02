c79361a2a26f671d08551db31b418c724e4f5f64
RB <rkrb@tyfonmail.se>
2024-08-02 14:41:41 +0200

    removed unused functions, CRUD was done differently

--- data.ts
+++ data.ts
@@ -1,20 +1,7 @@
-import {IMovie, IMovieCollection} from "./interfaces";
+import {IMovie} from "./interfaces";
 
 export function AddMovieToArray(movieCollection: IMovie[], newMovie: IMovie): IMovie[] {
     return [...movieCollection, newMovie]
-}
-
-export const GetMovieCollection = (movies: IMovie[]): IMovieCollection => (
-    {
-        collection: movies
-    });
-
-export function SetMovie(setMovie: (movie: IMovie) => void, {id}: { id: number }) {
-    const {collection} = GetMovieCollection([]);
-    const targetMovie = collection.find(movie => movie.id === id);
-    if (targetMovie) {
-        setMovie(targetMovie);
-    }
 }
 
 export function DeleteMovieByIndex(movies: IMovie[], index: number): IMovie[] {
